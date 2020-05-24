620749fab7f2cbd0da8a7dfca940e6f586327cb1
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-24 20:21:26 +0100

    added conditional statements to util functions to make sure the necessary code blocks were being accessed when needed whilst seeding

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,28 +1,40 @@
 exports.formatDates = (list) => {
-  return list.map((obj) => {
-    const unformattedTimestamp = obj.created_at;
-    obj.created_at = new Date(unformattedTimestamp);
-    return obj;
-  });
+  if (list.length === 0) return [];
+  else {
+    return list.map((obj) => {
+      const unformattedTimestamp = obj.created_at;
+      obj.created_at = new Date(unformattedTimestamp);
+      return obj;
+    });
+  }
 };
 
 exports.makeRefObj = (list) => {
   return list.reduce((refObj, listItem) => {
-   refObj[listItem.title] = listItem.article_id
-   return refObj
-  }, {})
+    refObj[listItem.title] = listItem.article_id;
+    return refObj;
+  }, {});
 };
 
 exports.formatComments = (comments, articleRef) => {
-  const formattedComment =  comments.map((comment) => {
-    const belongsToValue = comment.belongs_to
+  if (!comments) return [];
+  else {
+    const timeFormattedComments = this.formatDates(comments);
+    return timeFormattedComments.map((comment) => {
+      const belongsToValue = comment.belongs_to;
+      const {
+        created_by: author,
+        belongs_to: article_id,
+        ...restOfComment
+      } = comment;
 
-    const {created_by: author, belongs_to: article_id, ...restOfComment} = comment
-  
-    const newObj = {author, article_id: articleRef[belongsToValue], ...restOfComment}
+      const newObj = {
+        author,
+        article_id: articleRef[belongsToValue],
+        ...restOfComment,
+      };
 
-    return newObj
- 
-  })
-  return this.formatDates(formattedComment)
+      return newObj;
+    });
+  }
 };
