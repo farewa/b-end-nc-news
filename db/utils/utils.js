8e5733fb2661728e51611b01c1cfd3558e27401c
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-24 17:26:42 +0100

    completed formatDates function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,11 @@
-exports.formatDates = list => {};
+exports.formatDates = (list) => {
+  return list.map((obj) => {
+    const unformattedTimestamp = obj.created_at;
+    obj.created_at = new Date(unformattedTimestamp);
+    return obj;
+  });
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = (list) => {};
 
 exports.formatComments = (comments, articleRef) => {};
