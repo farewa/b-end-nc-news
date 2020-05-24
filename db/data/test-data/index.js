ed837ba762e44765cf18b2754c57b54b9c9a2019
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-24 16:41:21 +0100

    exporting test data

---
+++ db/data/test-data/index.js
@@ -0,0 +1,7 @@
+exports.articleData = require('./articles')
+exports.commentsData = require('./comments')
+exports.topicData = require('./topics')
+exports.userData = require('./users')
+
+
+// adding data for individual files onto the export object so that both dev and test data can be exported from one index.js file in the main data folder
