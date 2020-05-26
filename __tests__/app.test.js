b3b91ea3db1a8ffe2947265007c13c26d1217257
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-26 21:51:20 +0100

    made sure that node_env is set to test in test file for integration tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,20 @@
+process.env.NODE_ENV = "test";
 const {expect} = require('chai')
 const app = require('../app')
 const request = require('supertest')
 
-
+describe('/api', () => {
+  
+  describe('/topics', () => {
+    it('tests that GET returns a status of 200 and all of the topics', () => {
+      return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(({body}) => {
+        console.log(body)
+      })
+    });
+  });
+});
 
 
