de586d4bc28a4d19877654adfdb55b29e9dbb916
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-27 16:24:58 +0100

    added integration test for getting all topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,20 +1,25 @@
-process.env.NODE_ENV = "test";
-const {expect} = require('chai')
-const app = require('../app')
-const request = require('supertest')
+process.env.NODE_ENV = "test"; // setting environment to test
+const { expect } = require("chai");
+const app = require("../app"); // the app that we want to test
+const request = require("supertest"); // allows us to mimic url request
+const connection = require("../db/connection"); //
 
-describe('/api', () => {
-  
-  describe('/topics', () => {
-    it('tests that GET returns a status of 200 and all of the topics', () => {
+describe("/api", () => {
+  beforeEach(() => connection.seed.run()); // allows database to be reseeded before every test
+  after(() => connection.destroy()); // stops tests hanging
+
+  describe("/topics", () => {
+    it("tests that GET returns a status of 200 and all of the topics", () => {
       return request(app)
-      .get('/api/topics')
-      .expect(200)
-      .then(({body}) => {
-        console.log(body)
-      })
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body: { topics } }) => {
+          expect(topics.length).to.equal(3);
+          expect(topics[0]).to.eql({
+            slug: "mitch",
+            description: "The man, the Mitch, the legend",
+          });
+        });
     });
   });
 });
-
-
