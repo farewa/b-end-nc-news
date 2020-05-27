115037df2a620890f8311075e95fbeb428768121
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-27 17:09:29 +0100

    created userRouter

---
+++ routes/userRouter.js
@@ -0,0 +1,8 @@
+const userRouter = require('express').Router()
+const {getUserByUsername} = require('../controllers/user-controller')
+
+userRouter.route('/:username')
+.get(getUserByUsername)
+
+
+module.exports = userRouter
