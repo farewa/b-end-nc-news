5167117a414616e8634097c1dd2aadcb3cc931ad
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 21:04:37 +0100

    added catch to the patchArticleById controller

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -1,14 +1,18 @@
-const {fetchArticleById, updateArticleById} = require('../models/article-models')
+const {
+  fetchArticleById,
+  updateArticleById,
+} = require("../models/article-models");
 
 exports.getArticleById = (req, res, next) => {
-  const {article_id} = req.params
+  const { article_id } = req.params;
   fetchArticleById(article_id)
-    .then((article) => res.status(200).send({article}))
-    .catch(next)
-}
+    .then((article) => res.status(200).send({ article }))
+    .catch(next);
+};
 exports.patchArticleById = (req, res, next) => {
- const {inc_votes} = req.body
- const {article_id} = req.params
- updateArticleById(article_id, inc_votes)
-  .then((article) => res.status(200).send({article}))
-}
+  const { inc_votes } = req.body;
+  const { article_id } = req.params;
+  updateArticleById(article_id, inc_votes)
+  .then((article) => res.status(200).send({ article }))
+  .catch(next)
+};
