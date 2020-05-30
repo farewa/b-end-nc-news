74a25eb74de6d510744359847c243ae73cdee937
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 17:13:02 +0100

    modified userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,10 @@
 const userRouter = require('express').Router()
 const {getUserByUsername} = require('../controllers/user-controller')
+const {methodNotAllowed} = require('../errors')
 
 userRouter.route('/:username')
 .get(getUserByUsername)
+.all(methodNotAllowed)
 
 
 module.exports = userRouter
