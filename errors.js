51332fc822ef4c24660966e181f1186e394f4cf4
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 18:37:44 +0100

    added not found error handler

--- errors.js
+++ errors.js
@@ -1,17 +1,20 @@
 exports.routeNotFound = (req, res, next) => {
-  console.log(err)
-  next(err)
-}
+  console.log(err);
+  next(err);
+};
 
 exports.methodNotAllowed = (req, res) => {
-  console.log(err)
-  res.status(405).send({msg: 'Method Not Allowed'})
-}
+  console.log(err);
+  res.status(405).send({ msg: "Method Not Allowed" });
+};
 
 exports.badRequest = (err, req, res, next) => {
-  const errCodes = ['22P02']
+  const errCodes = ["22P02"];
   if (errCodes.includes(err.code)) {
-    res.status(400).send({message: err.msg || "Bad Request" })
-  }
-  else next(err)
-}
+    res.status(400).send({ message: err.msg || "Bad Request" });
+  } else next(err);
+};
+
+exports.notFound = (err, req, res, next) => {
+  res.status(404).send({message: err.message});
+};
