2625bfe5512f235abf7b2609877173f6626a25d4
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 20:31:18 +0100

    made controller for updated the votes on an article

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -1,8 +1,14 @@
-const {fetchArticleById} = require('../models/article-models')
+const {fetchArticleById, updateArticleById} = require('../models/article-models')
 
 exports.getArticleById = (req, res, next) => {
   const {article_id} = req.params
   fetchArticleById(article_id)
     .then((article) => res.status(200).send({article}))
     .catch(next)
+}
+exports.patchArticleById = (req, res, next) => {
+ const {inc_votes} = req.body
+ const {article_id} = req.params
+ updateArticleById(article_id, inc_votes)
+  .then((article) => res.status(200).send({article}))
 }
