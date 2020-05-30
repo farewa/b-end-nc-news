808c68562894b8f87b9f40425936725dbc29337c
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 16:20:31 +0100

    added routing for article endpoint

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const apiRouter = require('./routes/apiRouter')
+const {routeNotFound} = require('./errors')
 const app = express()
 
 
 app.use('/api', apiRouter)
+
+app.use('/*', routeNotFound)
+
 
 
 
