a413c417a0df437acc66f265ac47301fed0032bb
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 21:44:37 +0100

    added new route to articleRouter in order to add a comment to a specific article_id

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,8 +1,12 @@
 const articleRouter = require('express').Router()
-const { getArticleById, patchArticleById} = require('../controllers/article-controllers')
+const { getArticleById, patchArticleById, postCommentByArticleId} = require('../controllers/article-controllers')
 
 articleRouter.route('/:article_id')
 .get(getArticleById)
 .patch(patchArticleById)
+
+
+articleRouter.route('/:article_id/comments')
+.post(postCommentByArticleId)
 
 module.exports = articleRouter
