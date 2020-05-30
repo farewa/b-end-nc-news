a541f6475812c99d82cd26c14d4b9da5f8ac2f9b
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 20:28:57 +0100

    added express.json() so I could make a patch request

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const apiRouter = require('./routes/apiRouter')
 const {routeNotFound, badRequest, notFound} = require('./errors')
+
 const app = express()
 
+app.use(express.json())
 
 app.use('/api', apiRouter)
 
