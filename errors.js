b795ec444811a7a5488f2bc2cfc68bdbef3740e3
ibi30 <46229709+ibi30@users.noreply.github.com>
2020-05-30 18:13:33 +0100

    added error handler for bad requests

--- errors.js
+++ errors.js
@@ -1,4 +1,4 @@
-exports.routeNotFound = (err, req, res, next) => {
+exports.routeNotFound = (req, res, next) => {
   console.log(err)
   next(err)
 }
@@ -6,4 +6,12 @@ exports.routeNotFound = (err, req, res, next) => {
 exports.methodNotAllowed = (req, res) => {
   console.log(err)
   res.status(405).send({msg: 'Method Not Allowed'})
+}
+
+exports.badRequest = (err, req, res, next) => {
+  const errCodes = ['22P02']
+  if (errCodes.includes(err.code)) {
+    res.status(400).send({message: err.msg || "Bad Request" })
+  }
+  else next(err)
 }
