3d4f9a95aa95a9eadd708de78a9b1cf9e9cd81ab
ibi30 <user@Users-MacBook-Air.local>
2020-06-08 20:26:51 +0100

    added controller for getting all articles

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -1,9 +1,15 @@
 const {
+  fetchAllArticles,
   fetchArticleById,
   updateArticleById,
   insertCommentByArticleId,
   fetchCommentByArticleId
 } = require("../models/article-models");
+
+exports.getAllArticles = (req, res, next) => {
+  fetchAllArticles()
+    .then((articles) => res.status(200).send({articles}))
+}
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
