20e6412575ea44522cff2004a21a9fa3d08aca82
ibi30 <user@Users-MacBook-Air.local>
2020-06-11 13:01:48 +0100

    reorganised error file

--- errors.js
+++ errors.js
@@ -1,27 +1,30 @@
 exports.routeNotFound = (req, res, next) => {
   console.log('HERE in notFound')
   res.status(404).send('Route not found');
+  next(err)
+};
+
+exports.methodNotAllowed = (req, res, next) => {
+  console.log('in method not allowed');
+  res.status(405).send({ message: "Method Not Allowed" });
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
   console.log('here IN custom')
-  console.log(err.code);
+  console.log(err.status);
   if (err.status) res.status(err.status).send({message: err.message})
   else next(err);
 };
 
 exports.handlePSQLErrors = (err, req, res, next) => {
-  // console.log(err, 'handle')
+  console.log( 'handle')
   const err400Codes = ["22P02","42703"];
   if (err400Codes.includes(err.code)) {
     res.status(400).send({ message: err.msg || "Bad Request" });
   } else next(err);
 };
 
-exports.methodNotAllowed = (req, res, next) => {
-  // console.log(err);
-  res.status(405).send({ message: "Method Not Allowed" });
-};
+
 
 
 
