46c62e825ecdec45afc45c71cab71abb03dc7c1c
ibi30 <user@Users-MacBook-Air.local>
2020-06-11 13:05:04 +0100

    added .all to all routes in topic router to use methodNotAllowed middleware

--- routes/topicRouter.js
+++ routes/topicRouter.js
@@ -1,7 +1,9 @@
 const topicRouter = require('express').Router()
 const {getTopics} = require('../controllers/topic-controllers')
+const {methodNotAllowed} = require('../errors')
 
 topicRouter.route('/')
 .get(getTopics)
+.all(methodNotAllowed)
 
 module.exports = topicRouter 
