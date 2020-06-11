19152e7c36b8aaaca0ab419619c42efa9c601138
ibi30 <user@Users-MacBook-Air.local>
2020-06-11 12:17:53 +0100

    added catch block to getAllArticles controller

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -10,6 +10,7 @@ exports.getAllArticles = (req, res, next) => {
   const {sort_by, order, author, topic} = req.query
   fetchAllArticles(sort_by, order, author, topic)
     .then((articles) => res.status(200).send({articles}))
+    .catch(next)
 }
 
 exports.getArticleById = (req, res, next) => {
