0d90a808038f8cfb07d902880eace384e9d8add2
ibi30 <user@Users-MacBook-Air.local>
2020-06-11 13:04:16 +0100

    add .all to all routes to use methodNotAllowed middleware

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,17 +1,20 @@
 const articleRouter = require('express').Router()
 const { getArticleById, patchArticleById, postCommentByArticleId, getCommentByArticleId, getAllArticles} = require('../controllers/article-controllers')
+const {methodNotAllowed} = require('../errors')
 
 articleRouter.route('/')
 .get(getAllArticles)
+.all(methodNotAllowed)
 
 articleRouter.route('/:article_id')
 .get(getArticleById)
 .patch(patchArticleById)
-
+.all(methodNotAllowed)
 
 articleRouter.route('/:article_id/comments')
 .post(postCommentByArticleId)
 .get(getCommentByArticleId)
+.all(methodNotAllowed)
 
 
 module.exports = articleRouter
