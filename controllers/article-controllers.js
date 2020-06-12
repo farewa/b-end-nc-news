65dd8e70dec0efc25f2082acc11e75deaa2ed8a1
ibi30 <user@Users-MacBook-Air.local>
2020-06-12 12:04:21 +0100

    formatted the file

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -9,8 +9,6 @@ const {
 exports.getAllArticles = (req, res, next) => {
   const {sort_by, order, author, topic} = req.query
   fetchAllArticles(sort_by, order, author, topic)
-
-  
     .then((articles) => res.status(200).send({articles}))
     .catch(next)
 }
