bc791bea41a97e3cf447af3476990971afdea718
ibi30 <user@Users-MacBook-Air.local>
2020-06-12 12:48:01 +0100

    created commentRouter

---
+++ routes/commentRouter.js
@@ -0,0 +1,8 @@
+const commentRouter = require('express').Router()
+const {patchCommentById} = require('../controllers/comment-controller')
+
+commentRouter.route('/:comment_id')
+.patch(patchCommentById)
+
+
+module.exports = commentRouter
