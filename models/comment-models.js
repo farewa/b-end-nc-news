6bb513476c6b970203bf1848fe1aeed5c13b7623
ibi30 <user@Users-MacBook-Air.local>
2020-06-12 14:19:59 +0100

    created removeCommentById model

--- models/comment-models.js
+++ models/comment-models.js
@@ -7,3 +7,12 @@ exports.updateCommentById = (comment_id, inc_votes) => {
     .returning('*')
     .then(([article]) => article)
 }
+
+exports.removeCommentById = (comment_id) => {
+    return connection('comments')
+    .where('comment_id', comment_id)
+    .delete()
+    .then((response) => {
+        if (response) return true
+    })
+}
