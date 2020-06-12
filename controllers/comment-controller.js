bdea83418df57fe1d0fc9f40a6decc75a26f1b41
ibi30 <user@Users-MacBook-Air.local>
2020-06-12 14:19:29 +0100

    created deleteCommentById controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,8 +1,14 @@
-const {updateCommentById} = require('../models/comment-models')
+const {updateCommentById, removeCommentById} = require('../models/comment-models')
 
 exports.patchCommentById = (req, res, next) => {
     const {comment_id} = req.params
     const {inc_votes} = req.body
     updateCommentById(comment_id, inc_votes)
     .then(comment => res.status(201).send({comment}))
+}
+
+exports.deleteCommentById = (req, res, next) => {
+    const {comment_id} = req.params
+    removeCommentById(comment_id)
+    .then(() => res.sendStatus(204))
 }
