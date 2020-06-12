c9e309089514fdc50b47ccf13d4fe827d3e25a8b
ibi30 <user@Users-MacBook-Air.local>
2020-06-12 15:21:18 +0100

    added catch blocks to comment controllers

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -4,7 +4,7 @@ exports.patchCommentById = (req, res, next) => {
     const {comment_id} = req.params
     const {inc_votes} = req.body
     updateCommentById(comment_id, inc_votes)
-    .then(comment => res.status(201).send({comment}))
+    .then(comment => res.status(200).send({comment}))
     .catch(next)
 }
 
