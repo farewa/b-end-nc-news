4f1676d8fb9399466922a144f07f7f2f771c4144
ibi30 <user@Users-MacBook-Air.local>
2020-06-13 12:32:36 +0100

    added some error handling to updateCommentById model

--- models/comment-models.js
+++ models/comment-models.js
@@ -1,11 +1,14 @@
 const connection = require('../db/connection')
 
-exports.updateCommentById = (comment_id, inc_votes) => {
+exports.updateCommentById = (comment_id, inc_votes = 0) => {
     return connection('comments')
     .where('comment_id', comment_id)
     .increment({votes: inc_votes})
     .returning('*')
-    .then(([article]) => article)
+    .then(([article]) => {
+        if (!article) return Promise.reject({status: 404, message: 'Route not found'})
+        else return article
+    })
 }
 
 exports.removeCommentById = (comment_id) => {
