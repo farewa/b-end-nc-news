523928f4343b87eaab10d9c680bf1ea6ba794d65
ibi30 <user@Users-MacBook-Air.local>
2020-06-13 14:36:30 +0100

    removed console.log's from error handlers

--- errors.js
+++ errors.js
@@ -1,16 +1,8 @@
-exports.routeNotFound = (req, res, next) => {
-  console.log('HERE in notFound')
-  res.status(404).send('Route not found');
-  next(err)
-};
-
 exports.methodNotAllowed = (req, res, next) => {
-  console.log('in method not allowed');
   res.status(405).send({ message: "Method Not Allowed" });
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
-  console.log('here IN custom')
   if (err.status) res.status(err.status).send({message: err.message})
   else next(err);
 };
