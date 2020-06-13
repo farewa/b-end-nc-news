5a7bbe50ca9724ac31cc4b090959fff9cb109b68
ibi30 <user@Users-MacBook-Air.local>
2020-06-13 15:26:30 +0100

    renamed the file and added test on /api/articles/:article_id/comments to test that a valid article_id that does not have associated comments doesn't error

---
+++ tests/utils.test.js
@@ -0,0 +1,128 @@
+const { expect } = require("chai");
+const {
+  formatDates,
+  makeRefObj,
+  formatComments,
+} = require("../db/utils/utils");
+
+describe("formatDates", () => {
+  it("takes an empty array and returns an empty new array", () => {
+    expect(formatDates([])).to.eql([]);
+  });
+  it("takes an array with one object and converts the timestamp value to a JavaScript date object", () => {
+    const list = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+    ];
+    const formattedList = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: new Date(1542284514171),
+        votes: 100,
+      },
+    ];
+    expect(formatDates(list)).to.eql(formattedList);
+  });
+  it("takes an array with multiple objects and converts the timestamp value to a JavaScript date object", () => {
+    const list = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: 1416140514171,
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: 1289996514171,
+      },
+    ];
+    const formattedList = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: new Date(1542284514171),
+        votes: 100,
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: new Date(1416140514171),
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: new Date(1289996514171),
+      },
+    ];
+    expect(formatDates(list)).to.eql(formattedList);
+  });
+});
+
+describe("makeRefObj", () => {
+  it("returns an empty object when passed an empty array", () => {
+    expect(makeRefObj([])).to.eql({});
+  });
+  it("returns a reference object made up of data from the array that is passed into the function ", () => {
+    const list = [{ article_id: 1, title: "A" }];
+    const refObj = { A: 1 };
+    expect(makeRefObj(list)).to.eql(refObj);
+  });
+});
+
+describe("formatComments", () => {
+  it("returns an empty array when passed an empty array and an empty reference object ", () => {
+    expect(formatComments([], {})).to.eql([]);
+  });
+  it("returns a formatted comment array when passed an array with one comment object and a reference object", () => {
+    const comment = [{
+      body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: "butter_bridge",
+      votes: 16,
+      created_at: 1511354163389,
+    }];
+    const articleRef = { "They're not exactly dogs, are they?": 1 };
+    const formattedComment = [{
+      body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      article_id: 1,
+      author: "butter_bridge",
+      votes: 16,
+      created_at: new Date(1511354163389),
+    }];
+    expect(formatComments(comment, articleRef)).to.eql(formattedComment)
+  });
+  
+  // do I need one more test?
+});
+
