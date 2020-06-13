4300dc7fca0511c6ac23c23c3edfffc0379bc448
ibi30 <user@Users-MacBook-Air.local>
2020-06-13 21:08:35 +0100

    added on Delete cascade to article_id column

--- db/migrations/20200524163033_create_comment_table.js
+++ db/migrations/20200524163033_create_comment_table.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('comments', (commentTable) => {
     commentTable.increments('comment_id').primary()
     commentTable.string('author').references('users.username').notNullable()
-    commentTable.integer('article_id').references('articles.article_id')
+    commentTable.integer('article_id').references('articles.article_id').onDelete('CASCADE')
     commentTable.integer('votes').defaultTo(0)
     commentTable.timestamp('created_at').defaultTo(knex.fn.now())
     commentTable.text('body').notNullable()
