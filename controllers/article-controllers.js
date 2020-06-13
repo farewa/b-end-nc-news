c0fcc4c3fd84291278c4814c6d8faf642d96f4a2
ibi30 <user@Users-MacBook-Air.local>
2020-06-13 14:38:34 +0100

    removed readMe's from files

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -19,6 +19,7 @@ exports.getArticleById = (req, res, next) => {
     .then((article) => res.status(200).send({ article }))
     .catch(next);
 };
+
 exports.patchArticleById = (req, res, next) => {
   const { article_id } = req.params;
   const { inc_votes } = req.body;
