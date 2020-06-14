243b78555539e3c1c1b7c5232dfb28b8906540c8
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:54:53 +0100

    created insertNewUser model

--- models/user-models.js
+++ models/user-models.js
@@ -10,3 +10,15 @@ exports.fetchUserByUsername = (username) => {
       else return user
     })
 };
+
+
+exports.insertNewUser = (newUser) => {
+  if (!newUser.username ||!newUser.name === undefined) return Promise.reject({status: 400, message: 'Bad Request'})
+  return connection('users')
+  .insert(newUser)
+  .returning('*')
+  .then(([user]) => {
+    if (!user) return Promise.reject({status: 404, message: 'Route not found'})
+    else return user
+  })
+}
