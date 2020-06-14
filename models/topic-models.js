84941337adcb2c60f968b08cc2e3d21870859566
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:22:27 +0100

    added error handling to insertTopic model

--- models/topic-models.js
+++ models/topic-models.js
@@ -5,8 +5,12 @@ exports.sendTopics = () => {
 };
 
 exports.insertTopic = (newTopic) => {
+  if (Object.values(newTopic).includes(undefined)) return Promise.reject({status: 400, message: 'Bad Request'})
   return connection('topics')
   .insert(newTopic)
   .returning('*')
-  .then(([topic]) => topic)
+  .then(([topic]) => {
+    if (!topic) return Promise.reject({status: 404, message: 'Route not found'})
+    else return topic
+  })
 }
