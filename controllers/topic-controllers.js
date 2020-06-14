44c2d2ad3813ccbd602b631bb56a26108877e4fc
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:12:59 +0100

    created postTopic controller

--- controllers/topic-controllers.js
+++ controllers/topic-controllers.js
@@ -1,7 +1,14 @@
-const {sendTopics} = require('../models/topic-models')
+const {sendTopics, insertTopic} = require('../models/topic-models')
 
 exports.getTopics = (req, res, next) => {
   sendTopics()
     .then((topics) => res.status(200).send({topics}))
     .catch(next)
+}
+
+exports.postTopic = (req, res, next) => {
+  const {slug, description} = req.body
+  const newTopic = {slug, description}
+  insertTopic(newTopic)
+  .then((topic) => res.status(201).send({topic}))
 }
