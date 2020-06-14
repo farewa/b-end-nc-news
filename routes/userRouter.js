c6fd0204b73cafc58851f1e0997a8fa1e25966e7
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:52:36 +0100

    created new route on userRouter so that you can add a new user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,10 @@
 const userRouter = require('express').Router()
-const {getUserByUsername} = require('../controllers/user-controller')
+const {getUserByUsername, postNewUser} = require('../controllers/user-controller')
 const {methodNotAllowed} = require('../errors')
+
+userRouter.route('/')
+.post(postNewUser)
+.all(methodNotAllowed)
 
 userRouter.route('/:username')
 .get(getUserByUsername)
