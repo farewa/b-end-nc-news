026b8322999fbcbcb8a7b6ea6caf4f30a6e18c16
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:13:38 +0100

    created insertTopic model that sucessfully inserts new topic

--- models/topic-models.js
+++ models/topic-models.js
@@ -3,3 +3,10 @@ const connection = require("../db/connection");
 exports.sendTopics = () => {
   return connection.select("*").from("topics").returning("*");
 };
+
+exports.insertTopic = (newTopic) => {
+  return connection('topics')
+  .insert(newTopic)
+  .returning('*')
+  .then(([topic]) => topic)
+}
