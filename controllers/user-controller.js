15e601f325d22098b814a903d62d88c13cb1bc36
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:53:42 +0100

    created postUser controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,17 @@
-const {fetchUserByUsername} = require('../models/user-models')
+const {fetchUserByUsername, insertNewUser} = require('../models/user-models')
 
 
 exports.getUserByUsername = (req, res, next) => {
   const {username} = req.params
   fetchUserByUsername(username)
   .then((user) => res.status(200).send({user}))
+  .catch(next)
+}
+
+exports.postNewUser = (req, res, next) => {
+  const {username, avatar_url, name} = req.body
+  const newUser = {username, avatar_url, name}
+  insertNewUser(newUser)
+  .then((user) => res.status(201).send({user}))
   .catch(next)
 }
