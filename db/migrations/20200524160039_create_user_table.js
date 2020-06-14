4efd94d502d8551befd5bf806a617ef14296fc68
ibi30 <user@Users-MacBook-Air.local>
2020-06-14 17:50:26 +0100

    removed notNullable() from avatar_url column so that you can post a new user without a avater url

--- db/migrations/20200524160039_create_user_table.js
+++ db/migrations/20200524160039_create_user_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (userTable) => {
     userTable.string("username").unique().primary();
-    userTable.string("avatar_url").notNullable();
+    userTable.string("avatar_url")
     userTable.string("name").notNullable();
   });
 };
