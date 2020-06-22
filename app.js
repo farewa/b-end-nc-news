7e1275438a6a091f55504fb2000801ee8751b1dd
ibi30 <user@Users-MacBook-Air.local>
2020-06-22 22:09:54 +0100

    enabed CORS

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const apiRouter = require('./routes/apiRouter')
 const {handleCustomErrors, handlePSQLErrors} = require('./errors')
 
+const cors = require('cors')
+
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
